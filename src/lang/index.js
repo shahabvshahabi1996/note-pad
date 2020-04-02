cff754bdd12b9ec41ffc8576d4456a7132cd5dc5
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2020-04-03 01:20:05 +0430

    ADD Farsi and English language to project

---
+++ src/lang/index.js
@@ -0,0 +1,7 @@
+import En from "./locales/en_US";
+import Fr from "./locales/fr_IR";
+
+export default {
+  en: En,
+  fr: Fr
+};
---
+++ src/lang/locales/en_US.js
@@ -0,0 +1,6 @@
+export default {
+  locale: "en",
+  messages: {
+    "app.name": "Note Pad"
+  }
+};
