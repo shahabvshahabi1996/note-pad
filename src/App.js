13865ac9ad0da9489a18065f64311c4458b76000
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2020-04-04 01:12:14 +0430

    FIX UI Bugses

--- src/App.js
+++ src/App.js
@@ -2,19 +2,27 @@ import React, { Suspense } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import ThemeProvider from "./components/ThemeProvider";
 import { CircularProgress } from "@material-ui/core";
-
+import Container from "./components/Container";
 const lazyHome = React.lazy(() => import("./pages/home"));
 
 function App() {
   return (
     <ThemeProvider>
-      <BrowserRouter>
-        <Suspense fallback={<CircularProgress />}>
-          <Switch>
-            <Route exact path="/" component={lazyHome} />
-          </Switch>
-        </Suspense>
-      </BrowserRouter>
+      <Container>
+        <BrowserRouter>
+          <Suspense
+            fallback={
+              <div className="loader">
+                <CircularProgress />
+              </div>
+            }
+          >
+            <Switch>
+              <Route exact path="/" component={lazyHome} />
+            </Switch>
+          </Suspense>
+        </BrowserRouter>
+      </Container>
     </ThemeProvider>
   );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,71 @@
 import React from "react";
-import Container from "../components/Container";
+import { connect } from "react-redux";
+import { IconButton } from "@material-ui/core";
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteIcon from "@material-ui/icons/Delete";
+import { makeStyles } from "@material-ui/core/styles";
+import marked from "marked";
 
-export default () => {
+marked.setOptions({
+  gfm: true,
+  tables: true,
+  breaks: true,
+  pedantic: false,
+  sanitize: true,
+  smartLists: true,
+  smartypants: false
+});
+
+const useStyle = makeStyles(theme => ({
+  Icon: {
+    color: "#333"
+  }
+}));
+
+/**
+ * data -> {
+ * id : string,
+ * title : string,
+ * content : string
+ * }
+ *
+ */
+
+const Home = ({ data }) => {
+  const [edit, setEdit] = React.useState(false);
+
+  const toggleEdit = () => {
+    return setEdit(!edit);
+  };
+
+  const classes = useStyle();
   return (
-    <Container>
-      <div>Home</div>
-    </Container>
+    <div className="p-30">
+      <h1 className="data-title">
+        Note #1{" "}
+        <div>
+          <IconButton onClick={toggleEdit} className={classes.Icon}>
+            <DeleteIcon />
+          </IconButton>
+          <IconButton onClick={toggleEdit} className={classes.Icon}>
+            <EditIcon />
+          </IconButton>
+        </div>
+      </h1>
+      <hr />
+      <div
+        dangerouslySetInnerHTML={{
+          __html: marked(`### Tasks \n  **Wake up** soon`)
+        }}
+      ></div>
+    </div>
   );
 };
+
+const mapStateToProps = state => {
+  return {
+    note: state.note
+  };
+};
+
+export default connect(mapStateToProps)(Home);
