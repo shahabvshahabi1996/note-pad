d98b9daf32002b973521cf9f4683632b7e76e6f0
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2020-04-04 01:11:32 +0430

    FIX container ui bugses

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,33 +1,77 @@
 import React from "react";
 import styles from "./style.conatiner";
-import { IconButton } from "@material-ui/core";
+import { IconButton, Tooltip } from "@material-ui/core";
 import NoteIcon from "@material-ui/icons/Notes";
 import ThemeToggleIcon from "@material-ui/icons/Brightness4";
+import AddNoteIcon from "@material-ui/icons/PostAdd";
+import { connect } from "react-redux";
+import PropsTypes from "prop-types";
+import { toggleTheme } from "../../actions/settings";
+import CustomInput from "../Form/Input";
 
-const Container = ({ children }) => {
+const Container = ({ children, settings, toggleTheme }) => {
   const classes = styles();
+  const [menuIsOpen, openMenu] = React.useState(false);
+
+  const toggleMenu = () => {
+    return openMenu(!menuIsOpen);
+  };
 
   return (
     <>
       <div className={classes.navbar}>
         <div>
-          <IconButton className={classes.navbarIcon}>
-            <NoteIcon />
-          </IconButton>
+          <Tooltip title={"Notes"}>
+            <IconButton onClick={toggleMenu} className={classes.navbarIcon}>
+              <NoteIcon />
+            </IconButton>
+          </Tooltip>
           <h3>NotePad</h3>
         </div>
         <div>
-          <IconButton className={classes.toggleThemeIcon}>
-            <ThemeToggleIcon />
-          </IconButton>
+          <Tooltip title={"Add Note"}>
+            <IconButton className={classes.Icon}>
+              <AddNoteIcon />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title={"Toggle Theme"}>
+            <IconButton
+              onClick={() => toggleTheme(settings.theme)}
+              className={classes.Icon}
+            >
+              <ThemeToggleIcon />
+            </IconButton>
+          </Tooltip>
         </div>
       </div>
       <div className={classes.conatiner}>
-        <div className={classes.menuContainer}>Menu</div>
+        <div
+          className={`${classes.menuContainer} ${menuIsOpen ? "active" : ""}`}
+        >
+          <div className={classes.searchBox}>
+            <CustomInput />
+          </div>
+          <div className={classes.notes}>
+            <div className={"active"}>Note #1</div>
+            <div>Note #2</div>
+          </div>
+        </div>
         <div className={classes.contentContainer}>{children}</div>
       </div>
     </>
   );
 };
 
-export default Container;
+Container.propTypes = {
+  children: PropsTypes.node.isRequired,
+  settings: PropsTypes.object.isRequired,
+  toggleTheme: PropsTypes.func.isRequired
+};
+
+const mapStateToProps = state => {
+  return {
+    settings: state.settings
+  };
+};
+
+export default connect(mapStateToProps, { toggleTheme })(Container);
--- src/components/Container/style.conatiner.js
+++ src/components/Container/style.conatiner.js
@@ -1,52 +1,92 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-export default makeStyles(theme => ({
-  navbar: {
-    width: "100%",
-    borderBottom: "1px solid #eee",
-    height: "auto",
-    padding: 10,
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-between",
-    "& div": {
+export default makeStyles(theme => {
+  return {
+    navbar: {
+      width: "100%",
+      borderBottom: "1px solid #eee",
+      height: "auto",
+      padding: 10,
       display: "flex",
-      alignItems: "center"
-    },
-    color: theme.palette.primary.contrastText,
-    backgroundColor: theme.palette.primary.main
-  },
-  navbarIcon: {
-    display: "none"
-  },
-  toggleThemeIcon: {
-    color: theme.palette.primary.contrastText
-  },
-  conatiner: {
-    display: "flex",
-    width: "100vw",
-    height: "90vh"
-  },
-  contentContainer: {
-    width: "80%",
-    height: "100%",
-    borderLeft: "1px solid #eee",
-    padding: theme.spacing(3)
-  },
-  menuContainer: {
-    width: "20%"
-  },
-  "@media screen and (max-width: 600px)": {
-    menuContainer: {
-      width: 0,
-      display: "none"
-    },
-    contentContainer: {
-      width: "100%"
+      alignItems: "center",
+      justifyContent: "space-between",
+      "& div": {
+        display: "flex",
+        alignItems: "center"
+      },
+      color: theme.palette.primary.contrastText,
+      backgroundColor: theme.palette.primary.main
     },
     navbarIcon: {
-      display: "initial",
+      display: "none"
+    },
+    Icon: {
       color: theme.palette.primary.contrastText
+    },
+    conatiner: {
+      display: "flex",
+      width: "100vw",
+      height: "90vh",
+      position: "relative"
+    },
+    contentContainer: {
+      width: "80%",
+      height: "100%",
+      borderLeft: "1px solid #eee"
+    },
+    menuContainer: {
+      width: "20%",
+      height: "90vh",
+      backgroundColor: "#fff"
+    },
+
+    notes: {
+      "& div": {
+        padding: theme.spacing(3),
+        borderBottom: "1px solid #eee",
+        cursor: "pointer",
+        transition: "color 0.3s , background-color 0.3s ",
+        "&:hover": {
+          backgroundColor: theme.palette.primary.main,
+          color: theme.palette.primary.contrastText,
+          fontWeight: "bold"
+        },
+        "&.active": {
+          backgroundColor: theme.palette.primary.main,
+          color: theme.palette.primary.contrastText,
+          fontWeight: "bold"
+        }
+      }
+    },
+    active: {},
+    searchBox: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      flexDirection: "column",
+      borderBottom: "1px solid #eee",
+      padding: "5px 2px"
+    },
+    "@media screen and (max-width: 600px)": {
+      menuContainer: {
+        position: "absolute",
+        zIndex: 1,
+        width: "60%",
+        top: 0,
+        left: -500,
+        borderRight: "1px solid #eee",
+        transition: "left 0.5s",
+        "&.active": {
+          left: 0
+        }
+      },
+      contentContainer: {
+        width: "100%"
+      },
+      navbarIcon: {
+        display: "initial",
+        color: theme.palette.primary.contrastText
+      }
     }
-  }
-}));
+  };
+});
